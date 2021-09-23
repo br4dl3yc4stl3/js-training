8f9ffdb2558fa50cc990b42704d27224d2598faf
Bradley Travis <br4dl3y42@gmail.com>
2021-09-23 10:26:15 -0400

    -A

--- scripts.js
+++ scripts.js
@@ -16,3 +16,27 @@ function capitalize() {
     let result = word[0].toUpperCase() + word.slice(1).toLowerCase();
     console.log(result);
 }
+
+function lastLetter() {
+    let word = prompt("Give me a word please: ", "");
+    let result = word.slice(word.length - 1);
+    console.log(result);
+}
+
+function fizzBuzz() {
+    let number = parseInt(prompt("Enter your number: ", ""));
+    for (let i = 1; i <= number; i++)  {
+        if (i % 3 === 0) {
+            console.log("Fizz");
+        }  
+        if (i % 5 === 0) {
+            console.log("Buzz")
+        }
+        if (i % 3 === 0 && i % 5 === 0) {
+            console.log("FizzBuzz")
+        }
+        else {
+            console.log(i);
+        }
+    }
+}
